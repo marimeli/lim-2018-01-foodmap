6597a56657a83e51c0a36d1d9b206608fbec2efe
marimeli <marimel.casas@gmail.com>
2018-08-15 04:47:49 -0500

    closes #11

--- src/js/app.js
+++ src/js/app.js
@@ -1,14 +1,36 @@
-/* $(document).ready(function() {
+//Función screen splash
+window.setTimeout(() => {
+    preview.remove()
+    home.style.display = 'block'
+  }, 2000);
+
+/*  Función filter */
+window.filterRestaurants = (restaurants, search) => {
+    console.log('Entré a filterFood');
+    console.log(restaurants); //toda la data
+    console.log(search); //lo que escribe el usuario
+
+    let indexRestaurant = Object.keys(restaurants);
+        console.log(indexRestaurant); //index de objetos del array
+
+        for (indexRestaurant in restaurants) {
+            if (restaurants.hasOwnProperty(indexRestaurant)) {
+                const objRestaurant = restaurants[indexRestaurant];
+                console.log(objRestaurant); //el objeto con todas sus propiedades
+            }
+        }
+};
+
+/*  Función filter by type of food */
+$(document).ready(function() {
     $(".filter-button").click(function(){
         var value = $(this).attr('data-filter');
         
-        if(value == "all")
-        {
+        if(value === "all") {
             //$('.filter').removeClass('hidden');
             $('.filter').show('1000');
         }
-        else
-        {
+        else {
 //            $('.filter[filter-item="'+value+'"]').removeClass('hidden');
 //            $(".filter").not('.filter[filter-item="'+value+'"]').addClass('hidden');
             $(".filter").not('.'+value).hide('3000');
@@ -22,4 +44,4 @@ $(this).removeClass("active");
 }
 $(this).addClass("active");
 });
- */
+
--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,33 @@
 const preview = document.getElementById('preview');
 const home = document.getElementById('home');
 const searchFood = document.getElementById('searcher');
-const cards = document.getElementById('cards');
+const gallery = document.getElementById('gallery');
+
+//Función que pinta la data en el documento
+const printRestaurants = (name, image) => {
+    gallery.innerHTML += `
+    <div class="gallery-product col-lg-4 col-md-4 col-sm-4 col-xs-6 filter hdpe">
+        <img src="../data/image/13.jpg "class="img-responsive" alt="img" width="365" height="365">
+        <span>${name}</span>
+        <button type="button" class="btn-modal" data-toggle="modal" data-target="#miModal">Ver más</button>
+    </div>
+    
+    <div class="modal fade" id="miModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+          <h4 class="modal-title" id="myModalLabel">${name}</h4>
+        </div>
+        <div class="modal-body">
+        Aquí va la descripción 
+        </div>
+      </div>
+    </div>
+  </div>`;
+};
 
 
 //Petición AJAX, usando el método GET para traer la data JSON.
@@ -19,37 +45,21 @@ fetch('../data/restaurants.json')
                 const objRestaurant = arrRestaurants[indexRestaurant];
                 console.log(objRestaurant); //el objeto con todas sus propiedades
                 console.log(objRestaurant.name); //objeto restaurant. name (propiedad)
+                console.log(objRestaurant.image);
                 /*   let restaurant = Object.values(objRestaurant);
                   console.log(restaurant); */
             }
         }
-    });
+        arrRestaurants.map(r => {
+            printRestaurants(r.name, r.adress, r.description, r.image);
+        });
 
-/*  Función filter */
-const filterRestaurants = (restaurants, search) => {
-    console.log('Entré a filterFood');
-    console.log(restaurants); //toda la data
-    console.log(search); //lo que escribe el usuario
-    /*  cards.innerHTML = ''; */
-
-    /*  let filteredRestaurants = restaurants.filter(restaurant =>
-         restaurant.name.toUpperCase().indexOf(search.toUpperCase()) > -1);
-     return filteredRestaurants;
-     console.log(filteredRestaurants); */
-
-    restaurants.filter(objRestaurant => {
-        if (objRestaurant.name.toUpperCase().includes(search.toUpperCase())) {
-            console.log(objRestaurant.name);
-        }
-    });
-
-};
-
+    })
+    .catch(error => console.log(error));
 
 /* Eventos del DOM */
 searchFood.addEventListener('keyup', () => {
     const searchValue = searchFood.value;
-    filterRestaurants(restaurants, searchValue);
+    filterRestaurants(arrRestaurants, searchValue);
 });
-
 
